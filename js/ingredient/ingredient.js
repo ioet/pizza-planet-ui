ab920e3c9b924e08a83ac34359cd3d0fe00233fc
David Cadena <david.cadena@hotmail.com>
2023-01-30 12:56:44 -0500

    refactor ingredient and size get method

--- js/ingredient/ingredient.js
+++ js/ingredient/ingredient.js
@@ -1,15 +1,8 @@
+import { insertDataToTable } from '../assets/index.js'
+
+const endpointName = "ingredient";
+const ingredientTemplate = "#ingredient-item-template";
+const tableId = "#ingredients";
 
 
-fetch('http://127.0.0.1:5000/ingredient')
-    .then(response => response.json())
-    .then(ingredients => {
-        let rows = ingredients.map(element => createIngredientTemplate(element));
-        let table = $("#ingredients tbody");
-        table.append(rows);
-    });
-
-
-function createIngredientTemplate(ingredient) {
-    let template = $("#ingredient-item-template")[0].innerHTML;
-    return Mustache.render(template, ingredient);
-}
+insertDataToTable(endpointName, ingredientTemplate, tableId);
