044b9334cf20b934f586b284f7ad1dc83c684290
Santiago220991 <alexander.cardenas10@outlook.com>
2023-03-13 18:48:03 -0500

    feat: add beverages section on the main view

--- js/index.js
+++ js/index.js
@@ -82,6 +82,16 @@ function fetchOrderSizes() {
         });
 }
 
+function fetchOrderBeverages() {
+    fetch('http://127.0.0.1:5000/beverage/')
+        .then(response => response.json())
+        .then(beverages => {
+            let rows = beverages.map(element => createBeverageTemplate(element));
+            let table = $("#beverages tbody");
+            table.append(rows);
+        });
+}
+
 function createIngredientTemplate(ingredient) {
     let template = $("#ingredients-template")[0].innerHTML;
     return Mustache.render(template, ingredient);
@@ -92,9 +102,15 @@ function createSizeTemplate(size) {
     return Mustache.render(template, size);
 }
 
+function createBeverageTemplate(beverage) {
+    let template = $("#beverages-template")[0].innerHTML;
+    return Mustache.render(template, beverage);
+}
+
 function loadInformation() {
     fetchIngredients();
     fetchOrderSizes();
+    fetchOrderBeverages();
 }
 
 
