3d2acaa660fc1553deb2200d9ad7b1f981b5b1b5
oliverreyesg <ing.oliverreyesgutierrez@gmail.com>
2023-08-17 15:06:13 -0400

    fix: order detail rendering

--- js/order/order-detail.js
+++ js/order/order-detail.js
@@ -18,5 +18,21 @@ fetch(`http://127.0.0.1:5000/order/id/${_id}`)
  */
 function createRowTemplate(order) {
     let template = $("#order-template")[0].innerHTML;
-    return Mustache.render(template, order);
+    const ingredients = filterIngredients(order)
+    const beverages = filterBeverages(order)
+    const mutated_order = JSON.parse(JSON.stringify(order))
+    mutated_order.ingredients = ingredients
+    mutated_order.beverages = beverages
+    return Mustache.render(template, mutated_order);
+}
+
+
+function filterBeverages(order) {
+    const beverages = order.detail.filter((detail) => detail.beverage && detail)
+    return beverages
+}
+
+function filterIngredients(order) {
+    const ingredients = order.detail.filter((detail) => detail.ingredient && detail)
+    return ingredients
 }
